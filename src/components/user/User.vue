55acd0503fb16fae75b04ba4b0bfcccf65a5e29a
zhaokangjin <270882007@qq.com>
2019-02-14 23:39:43 +0800

    新增一个组件

---
+++ src/components/user/User.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>{{message}}</h2>
+  </div>
+</template>
+<script>
+export default {
+  name: "Hi",
+  data() {
+    return {
+      message: "Wo Shi Hi!"
+    };
+  }
+};
+</script>
+<style scoped>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-
+import User from '../components/user/User.vue'
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+  routes: [{
+    path: '/',
+    name: 'HelloWorld',
+    component: HelloWorld
+  }, {
+    path: '/user',
+    name: 'user',
+    component: User
+  }]
 })
